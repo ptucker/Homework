5eaf3a921e1597b1e41b74b649159f85490a37b9
Peter Tucker <ptucker@whitworth.edu>
2019-01-06 13:16:59 -0800

    feedback

--- EX1_2/src/CircleArea.java
+++ EX1_2/src/CircleArea.java
@@ -6,6 +6,7 @@ public class CircleArea {
 public static void main(String[] args) {
 	System.out.println("Enter the Radius of a circle: ");
 	Scanner input = new Scanner(System.in);
+	//PT -- nextDouble would be better here. -1
 	double Radius = input.nextInt();
 	final double PI = 3.14;
 	double Area = (Radius * Radius * PI );
