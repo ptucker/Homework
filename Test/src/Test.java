a4be78432cf6120d9ea16f22df8ef04036925b39
Peter Tucker <ptucker@whitworth.edu>
2019-01-26 14:58:22 -0800

    Update Test.java

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -1,3 +1,21 @@
+/*
+FEEDBACK
+
+Read from the file correctly: 20/20%
+Implement class hierarchy correctly: 19/20%
+Populate appropriate data structures with shape data: 15/15%
+Show list of shapes correctly: 15/15%
+Show specific shape properties correctly: 14/15%
+Well-structured, well-commented code: 12/15%
+
+
+Comments:
+  output: Color: " ""red""" ???
+  ShapeInterface doesn't need to define toString(), since it's on Object
+  Why abstract Shape? It shouldn't implement constructors
+  Move readFile to it's own class
+*/
+
 import java.io.*;
 
 public class Test {
